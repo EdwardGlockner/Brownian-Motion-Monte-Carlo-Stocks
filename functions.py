8828ecdbf0f02d4c0549dff327d96a528f66da1c
EdwardGlockner <edward.glockner5@gmail.com>
2022-12-05 14:09:17 +0100

    read

--- functions.py
+++ functions.py
@@ -2,3 +2,23 @@ import pandas as pd
 import numpy as np
 from datetime import datetime
 import matplotlib.pyplot as plt
+
+
+def read_csv(file_path, tick):
+    """
+    @params:
+        file_path: path to the destination of the file
+        tick: ticker of the stock
+
+    @returns:
+        temp: pandas dataframe with closing price and date of the stock
+
+    Reads a file_path for a given ticker, and returns a dataframe with a column: close.
+    """
+    temp = pd.read_csv(file_path, parse_dates=[0], index_col=0)
+    temp.rename(columns={"Close" : tick}, inplace = True)
+    temp.drop(["Open", "High", "Low", "Adj Close", "Volume"], axis = 1, inplace = True)
+    return temp
+
+
+
--- main.py
+++ main.py
@@ -1,2 +1,9 @@
 from functions import *
 
+ticker = "AMZN"
+file_path = "/Users/edwardglockner/Library/CloudStorage/OneDrive-Uppsalauniversitet/Fristående Kurser/Inferensteori I/Brownian/Data/AMZN.csv"
+
+data = read_csv(file_path, ticker)
+
+
+
